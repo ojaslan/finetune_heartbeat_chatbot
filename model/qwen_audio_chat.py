8060edc54dfc15ce230f11abacb4c621a461aaef
Ojas lanjekar <152417288+ojaslan@users.noreply.github.com>
2026-01-31 02:55:41 +0530

    Update qwen_audio_chat.py

--- model/qwen_audio_chat.py
+++ model/qwen_audio_chat.py
@@ -1,19 +1,23 @@
+# model/qwen_audio_chat.py
+
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-MODEL_NAME = "Qwen/Qwen-Audio-Chat"
+MODEL_NAME = "Qwen/Qwen2-Audio-7B-Instruct"
 
 class QwenAudioChat:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained(
             MODEL_NAME,
             trust_remote_code=True,
-            use_fast=False   # 🔥 IMPORTANT FIX
+            use_fast=False
         )
 
         self.model = AutoModelForCausalLM.from_pretrained(
             MODEL_NAME,
             trust_remote_code=True,
+            torch_dtype=torch.float16,
             device_map="auto"
         ).eval()
 
-        self.history = None
+        self.history = []
