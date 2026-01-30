5be95ebe46c570f0424873991646dae9a4c41230
Ojas lanjekar <152417288+ojaslan@users.noreply.github.com>
2026-01-31 03:09:01 +0530

    Update audio_utils.py

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,16 +1,8 @@
 import librosa
 import soundfile as sf
-import numpy as np
 
-TARGET_SR = 16000
-
-def preprocess_audio(input_path, output_path="processed.wav"):
-    audio, sr = librosa.load(input_path, sr=None)
-
-    if sr != TARGET_SR:
-        audio = librosa.resample(audio, orig_sr=sr, target_sr=TARGET_SR)
-
-    audio = audio / np.max(np.abs(audio))  # normalize
-
-    sf.write(output_path, audio, TARGET_SR)
+def preprocess_audio(input_path):
+    y, sr = librosa.load(input_path, sr=16000, mono=True)
+    output_path = input_path.replace(".wav", "_processed.wav")
+    sf.write(output_path, y, 16000)
     return output_path
